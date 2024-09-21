8e4cfc4fab96771b9560edaef7d6b74cb68cc91c
evil-89789 <tngsurya055@gmail.com>
2024-09-21 13:43:29 +0545

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PrimaryNav from "./components/PrimaryNav";
 import SecondaryNav from "./components/SecondaryNav";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <SecondaryNav />
       <PrimaryNav />
       <Home />
+      <Footer />
     </>
   );
 };
