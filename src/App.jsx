2079b4a45e531278b115d65b7363a1fa9aa888b4
evil-89789 <tngsurya055@gmail.com>
2024-09-22 15:56:20 +0545

    login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PrimaryNav from "./components/PrimaryNav";
 import SecondaryNav from "./components/SecondaryNav";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import { Router, Routes, Route } from "react-router";
 
 const App = () => {
   return (
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signin = () => {
+  return (
+    <section>
+      <div>knaksnc</div>
+    </section>
+  );
+};
+
+export default Signin;
